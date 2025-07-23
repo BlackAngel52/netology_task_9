a981edfcc5bcd4f4a774212a0631f3c425624436
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 21:52:29 +0300

    Add files via upload

---
+++ netology_task_9_3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+void reverse(int *arr, int size) {
+	int temp{};
+
+	for (int i = 0; i < size / 2; ++i) {
+		temp = arr[i];
+		arr[i] = arr[size - 1 - i];
+		arr[size - 1 - i] = temp;
+	}
+
+}
+
+void print(int* arr, int size) {
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+}
+
+int main() {
+
+	int size{};
+	std::cout << "Enter array size" << std::endl;
+	std::cin >> size;
+
+
+	int* arr = new int[size];
+	for (int i = 0; i < size; ++i) {
+		arr[i] = i;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	reverse(arr, size);
+	print(arr, size);
+
+	delete[] arr;
+
+	return 0;
+}
