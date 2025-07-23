06154ab59fe7f5e18ae3c1708750dce0bc768431
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 21:33:05 +0300

    Add files via upload

---
+++ netology_task_9_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void swap(int* a, int* b) {
+	int temp{};
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+	int a{ 5 };
+	int b{ 10 };
+	int* ptrA = &a;
+	int* ptrB = &b;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap(ptrA, ptrB);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
